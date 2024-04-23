006790a396a9bf8c652d35826d9e0ccf890f78fc
Luiz Moura <moura.oliveira.luiz@gmail.com>
2024-04-22 20:04:20 -0400

    feat: create room page

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,14 +3,16 @@ import { Link } from '@inertiajs/react';
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-900">
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                    <h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white mb-7'>
+                        Movie <mark className='px-2 text-white bg-blue-600 rounded dark:bg-blue-500'>mach</mark>
+                    </h1>
                 </Link>
             </div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div className="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg">
                 {children}
             </div>
         </div>
--- resources/js/Pages/Room/Create.jsx
+++ resources/js/Pages/Room/Create.jsx
@@ -1,22 +1,55 @@
 import GuestLayout from '@/Layouts/GuestLayout';
 import { Head, useForm } from '@inertiajs/react';
+import { useEffect } from 'react';
+import { FiKey, FiHeart } from 'react-icons/fi';
+import { FaHeart } from 'react-icons/fa'
 
 export default function CreateRoom() {
-    const { data, setData, post, processing, errors, reset } = useForm()
+    const { post, processing } = useForm()
 
-    const submit = (e) => {
+    const createRoom = (e) => {
         e.preventDefault();
 
         post(route('room.store'));
     };
 
+    const enterRoom = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <GuestLayout>
-            <Head title="Movie Match" />
+            <Head title='Movie Match' />
 
-            <h2 onClick={submit}>Create Room</h2>
+            <p className='inline-flex items-center py-1 px-1 pe-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800 w-full'>
+                <span className='text-xs text-white px-4 py-1.5'><FaHeart color='red' className='text-lg'/></span>
+                <span className='text-sm font-medium'>Choose the best combination to watch now.</span>
+            </p>
 
-            <input type="text" />
+            <button
+                type='button'
+                className='text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-6 py-3.5 text-center me-2 mb-4  w-full'
+                onClick={createRoom}
+                disabled={processing}
+            >
+                Create your key
+            </button>
+
+            <form onSubmit={enterRoom}>
+                <label htmlFor='room-key' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Room key</label>
+                <div className='relative mb-6'>
+                    <div className='absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none'>
+                        <FiKey/>
+                    </div>
+                    <input
+                        type='text'
+                        name='room-key'
+                        id='room-key'
+                        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-4  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                        placeholder='Type your room key here...'
+                    />
+                </div>
+            </form>
         </GuestLayout>
     )
 }
