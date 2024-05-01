e6bf6502d41a61891f231897a6210aac7de85290
Luiz Moura <moura.oliveira.luiz@gmail.com>
2024-04-30 21:23:55 -0400

    feat: create animation for match component

--- resources/js/Components/Movies/Match.jsx
+++ resources/js/Components/Movies/Match.jsx
@@ -1,6 +1,12 @@
+import { motion } from 'framer-motion'
+
 export default function Match({ movie }) {
     return (
-        <div>
+        <motion.div
+            initial={{ scale: 0.5 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5 }}
+        >
             <figure className='relative max-w-sm transition-all duration-300 cursor-pointer'>
                 <a href='#'>
                     <img
@@ -11,10 +17,12 @@ export default function Match({ movie }) {
                 </a>
                 <figcaption className='absolute px-4 text-lg text-white text-center bottom-40 w-full flex justify-center'>
                     <div className="rounded-lg border-2 border-yellow-600 bg-black/70 p-2">
-                        <span className="text-3xl md:text-5xl lg:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r to-purple-700 from-yellow-500"><i>MATCH</i></span>
+                        <span className="text-3xl md:text-5xl lg:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r to-purple-700 from-yellow-500">
+                            <i>MATCH</i>
+                        </span>
                     </div>
                 </figcaption>
             </figure>
-        </div>
+        </motion.div>
     )
 }
