9bbed69552a277839c92e0175dba1ee93bc8ea02
Luiz Moura <moura.oliveira.luiz@gmail.com>
2024-05-13 13:41:09 -0400

    chore: add vite ssl basic and set https to true

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
     plugins: [
@@ -9,5 +10,9 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
+        basicSsl(),
     ],
+    server: {
+        https: true,
+    }
 });
